8023877c76e1ade9b7fb246e92978b3036e83250
satoru_kurita <rhodeus208@gmail.com>
2024-12-16 10:31:51 +0900

    prettierで整形した

--- kadai_002/nagoyameshi/demo/src/main/resources/static/js/category_num_count.js
+++ kadai_002/nagoyameshi/demo/src/main/resources/static/js/category_num_count.js
@@ -1,19 +1,12 @@
-
 const categoryCheckBoxes = document.querySelectorAll('.category-checkbox');
 
-
-
 //カテゴリの選択個数を5個までに抑える
-categoryCheckBoxes.forEach(box=>{
-    box.addEventListener('change',function(){
-        const checkedBox = document.querySelectorAll('.category-checkbox:checked');
-        if(checkedBox.length > 5){
-            this.checked = false;
-            alert('5つまでしか選択できません');
-        }
-    });
+categoryCheckBoxes.forEach((box) => {
+  box.addEventListener('change', function () {
+    const checkedBox = document.querySelectorAll('.category-checkbox:checked');
+    if (checkedBox.length > 5) {
+      this.checked = false;
+      alert('5つまでしか選択できません');
+    }
+  });
 });
-
-
-
-
